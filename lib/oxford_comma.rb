54293b600580fb21a6e4d3493d433d9a189005a5
Luke Pierotti <lpierotti2@gmail.com>
2017-06-12 23:09:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length <= 2
+    return array.join(" and ")
+  else
+    final = ""
+    array.each_with_index do |fruit, index|
+      if index + 1 == array.length
+        final << "and #{fruit}"
+      else
+        final << "#{fruit}, "
+      end
+    end
+  end
+  final
 end
